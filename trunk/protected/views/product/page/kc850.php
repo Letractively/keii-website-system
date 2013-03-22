1a3198c6993cba6c0b3aadc1aa448631f976016c
z.meroy@gmail.com <z.meroy@gmail.com@b6bd13aa-50f1-5fc7-e622-34943205cfec>
2013-03-22 14:59:32 +0000

    修改多个型号防震等级错误

--- trunk/protected/views/product/page/kc850.php
+++ trunk/protected/views/product/page/kc850.php
@@ -385,7 +385,7 @@ $this->pageTitle = Yii::app ()->name . ' - ' . Yii::t ( 'zii', 'kc850' );
 									防护等级																		
 									</td>
 									<td>
-									IP67，IEC 529，全金属外壳封装																	
+									IP54，IEC 529，全金属外壳封装
 									</td>
 								</tr>
 								<tr>
