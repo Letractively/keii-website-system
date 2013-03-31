9ae579234bb2ab5dd4583667e153741ff3e648b0
z.meroy@gmail.com <z.meroy@gmail.com@b6bd13aa-50f1-5fc7-e622-34943205cfec>
2013-03-31 09:00:34 +0000

    多语言化主页的幻灯片

---
+++ trunk/protected/views/site/index.content.slider.en.php
@@ -0,0 +1,14 @@
+<div class="clear" id="slider-bg">
+<div class="full-width-wrapper" id="slider-frame">
+<div class="fixed-width-wrapper maxx-theme" id="slider-wrapper">
+<div id="slider" class="nivoSlider">
+<?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/en/slider/slider01.jpg" alt="" title="">',array('/product/kc100')); ?>
+<?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/en/slider/slider02.jpg" alt="" title="">',array('/product/kc500')); ?>
+<?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/en/slider/slider03.jpg" alt="" title="">',array('/product/kc800')); ?>
+<?php echo CHtml::link('<img src="'.Yii::app()->request->baseUrl.'/images/en/slider/slider04.jpg" alt="" title="">',array('/product/substation')); ?>
+</div>
+</div>
+</div>
+</div>
+
+
