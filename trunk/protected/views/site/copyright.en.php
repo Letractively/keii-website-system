0a249fd791c78e17caaf0dd9a5d03dadb0b94561
z.meroy@gmail.com <z.meroy@gmail.com@b6bd13aa-50f1-5fc7-e622-34943205cfec>
2013-04-08 02:25:55 +0000

    修改样式

---
+++ trunk/protected/views/site/copyright.en.php
@@ -0,0 +1,6 @@
+<div id="copyright-en">
+<ul>
+<li>© 2013 - Guangzhou Keii Electro Optics Technology Co., Ltd</li>
+<li>ICP05093365</li>
+</ul>
+</div>
---
+++ trunk/protected/views/site/copyright.zh_cn.php
@@ -0,0 +1,6 @@
+<div id="copyright">
+<ul>
+<li>© 2013 - 广州科易光电技术有限公司</li>
+<li>ICP05093365</li>
+</ul>
+</div>
